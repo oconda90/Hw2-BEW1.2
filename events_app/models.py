48e392e5a24fe93aa140e9ffd96d354bd2b68c4c
oconda90 <70242240+oconda90@users.noreply.github.com>
2021-02-11 13:00:40 -0800

    finished all work

---
+++ events_app/models.py
@@ -0,0 +1,24 @@
+"""Create database models to represent tables."""
+from events_app import db
+from sqlalchemy.orm import backref
+
+
+class Guest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    events_attending = db.relationship("Event", secondary="guest_event", back_populates="guests")
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    date_and_time = db.Column(db.DateTime, nullable=False)
+    guests = db.relationship("Guest", secondary="guest_event", back_populates = "events_attending")
+
+
+guest_event_table = db.Table('guest_event',
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest.id')),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id')))
